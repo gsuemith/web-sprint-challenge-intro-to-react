a59c156152fd2eb68cfdee7fdd5efb8828db0cf1
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-19 07:53:57 -0500

    mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const characterDiv = styled.div`
+    text-align: justify;
+`
+
+const Character = ( { name }) => {
+    return (
+        <div>
+            <h4>{name}</h4>
+        </div>
+    )
+}
+
+export default Character
